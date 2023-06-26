619933ae5b01d9a6553b5f26d978eab63b78ad95
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2023-06-26 21:22:40 +0800

    🔧 [fix] some bugs & add new picture

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,16 @@
 // backgrounds
 const bg = [
-    "https://imgsrc.baidu.com/forum/pic/item/77c6a7efce1b9d16f8efebbdb6deb48f8d5464c5.jpg",
-    "https://imgsrc.baidu.com/forum/pic/item/b2de9c82d158ccbf4c97c4e65cd8bc3eb03541c6.jpg",
-    "https://imgsrc.baidu.com/forum/pic/item/f9dcd100baa1cd114e9203fcfc12c8fcc2ce2dc1.jpg",
-    "https://imgsrc.baidu.com/forum/pic/item/f3d3572c11dfa9ec2a0927cb27d0f703908fc1c3.jpg",
-    "https://imgsrc.baidu.com/forum/pic/item/0ff41bd5ad6eddc46157e3387cdbb6fd53663344.jpg",
-    "https://imgsrc.baidu.com/forum/pic/item/7e3e6709c93d70cf2352fdcebddcd100bba12b4c.jpg",
-    "https://imgsrc.baidu.com/forum/pic/item/d01373f082025aafed942908beedab64024f1a4a.jpg",
-    "https://imgsrc.baidu.com/forum/pic/item/fd039245d688d43faf0bc631381ed21b0ff43b56.jpg",
-    "https://imgsrc.baidu.com/forum/pic/item/9825bc315c6034a8ab70ef428e13495408237667.jpg"
-]
+    "77c6a7efce1b9d16f8efebbdb6deb48f8d5464c5",
+    "b2de9c82d158ccbf4c97c4e65cd8bc3eb03541c6",
+    "f9dcd100baa1cd114e9203fcfc12c8fcc2ce2dc1",
+    "f3d3572c11dfa9ec2a0927cb27d0f703908fc1c3",
+    "0ff41bd5ad6eddc46157e3387cdbb6fd53663344",
+    "7e3e6709c93d70cf2352fdcebddcd100bba12b4c",
+    "d01373f082025aafed942908beedab64024f1a4a",
+    "fd039245d688d43faf0bc631381ed21b0ff43b56",
+    "9825bc315c6034a8ab70ef428e13495408237667",
+    "b03533fa828ba61eea59d7e00434970a314e594d"
+].map(id=>`https://imgsrc.baidu.com/forum/pic/item/${id}.jpg`)
 
 if (!window.$) {
     window.$ = (selector) => document.querySelector(selector)
