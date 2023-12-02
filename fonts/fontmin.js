0d049cae5b0c82354ba7946b7b428566ff19401e
Kirikaze Chiyuki <me@chyk.ink>
2023-12-02 23:56:57 +0800

    💧 [water] 水 commit 以让 Cloudflare 自动部署

--- fonts/fontmin.js
+++ fonts/fontmin.js
@@ -1,25 +1,24 @@
 import Fontmin from "fontmin"
 
 const fontmin = new Fontmin()
-    .src('fonts/SmileySans-Oblique.ttf')
-    .dest('public/fonts')
-    .use(
-        Fontmin.glyph({
-            text: '千雪的咖啡厅'
-        })
-    )
-    .use(Fontmin.ttf2woff({deflate: true}))
-    .use(Fontmin.ttf2woff2())
-    .use(Fontmin.css({
-        fontPath: 'fonts/'
-    }));
-
+  .src('fonts/SmileySans-Oblique.ttf')
+  .dest('public/fonts')
+  .use(
+    Fontmin.glyph({
+      text: '千雪的咖啡厅'
+    })
+  )
+  .use(Fontmin.ttf2woff({deflate: true}))
+  .use(Fontmin.ttf2woff2())
+  .use(Fontmin.css({
+    fontPath: 'fonts/'
+  }))
 
 
 fontmin.run(function (err, files) {
-    if (err) {
-        throw err;
-    }
+  if (err) {
+    throw err
+  }
 
-    console.log(files[0]);
-});
+  console.log(files[0]);
+})
