081c4c0aaba14168fa7a024eb23965a4f069c1a8
Kirikaze Chiyuki <me@chyk.ink>
2024-09-18 16:00:22 +0800

    [fix] What's up?

--- src/i18n.js
+++ src/i18n.js
@@ -216,7 +216,7 @@ function _getLocaleId() {
     case "en-us":
     case "en":
     case "en-gb":
-      return "en-us";\
+      return "en-us";
     case "ja-jp":
     case "ja":
       return "ja-jp";
