4d8776deefa9423c7af08e0733c3a427edcf93e4
Kirikaze Chiyuki <me@chyk.ink>
2026-03-16 15:17:53 +0800

    进一步完善首页内容、添加 MDX 支持

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: true
-});
+  prefetch: true,
+  integrations: [mdx()],
+})
